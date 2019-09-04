5c7ac92119cb05cb7d0a3bde545a6ac61cb0ebc8
Chaenini <s2018w16@e-mirim.hs.kr>
2019-09-05 08:59:26 +0900

    foods.drinks 활용

---
+++ 패키지/main_drinks.py
@@ -0,0 +1,6 @@
+#from foods.drinks import milk, cola, water
+from foods.drinks import *
+
+milk.drink()    #꿀꺽꿀꺽~ 진하고 신선한 우유의 향연이~!
+cola.drink()    #톡톡 캬아 ~ 역시 콜라는 정만이네 콜라
+water.drink()   #맑고 투명한 물은 역시 지하수
