64a0c71b6c4e9d4ae36f3b0501f7b6d303d9d6bf
Chaenini <s2018w16@e-mirim.hs.kr>
2019-09-05 09:09:42 +0900

    foods.fruits 활용

---
+++ 패키지/main_fruits.py
@@ -0,0 +1,6 @@
+from foods.fruits import *
+#from foods.fruits import grape, peach, watermelon
+
+grape.eat()         #새콤한 맛도 있고 톡 이렇게 딱 터트리는 재미가 있다
+peach.eat()         #맛있다
+watermelon.eat()    #달고 시원하다
