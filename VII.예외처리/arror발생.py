84a95bb45977e0d328899d1c15a33cf890dabbca
Chaenini <s2018w16@e-mirim.hs.kr>
2019-09-23 14:20:23 +0900

    예외 처리

---
+++ VII.예외처리/arror발생.py
@@ -0,0 +1,5 @@
+#10/0    #ZeroDivisionError: division by zero - 0으로 나눌때
+spam = "hello"  #TypeError: unsupported operand type(s) for +: 'int' and 'str' - 자료형과 관련된 문제
+4 + spam * 3    #NameError: name 'spam' is not defined - 선언ㄴ되지 않은 변수에 접근
+#IOError - 입출력과 관련된 문제
+#ValueError - 잘못된 입력 값
