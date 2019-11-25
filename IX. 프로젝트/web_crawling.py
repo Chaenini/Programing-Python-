55b418152ac3eb551c70270f264938e89447a116
Chaenini <s2018w16@e-mirim.hs.kr>
2019-11-25 09:47:33 +0900

    Web Crawling - 간떨어지는동거 제목, 링크 가져오기 -> html 파일 만들기

--- IX. 프로젝트/web_crawling.py
+++ IX. 프로젝트/web_crawling.py
@@ -10,10 +10,17 @@ if __name__ == '__main__' :
         data = urlopen("https://comic.naver.com/webtoon/list.nhn?titleId=699415&weekday=thu")
         soup = BeautifulSoup(data, "lxml")  # httpResponse -> HTML
 
+        html = "<html><head><meta charset = 'utf-8'></head><body>"
         cartoon_titles = soup.find_all("td", attrs={"class": "title"})  # <td class = "title"> ... </td>
         for cartoon_title in cartoon_titles:    # cartoon_titles[:2] - 최근꺼 2개만 보기
             title = cartoon_title.find("a").text    # <a> text </a> - 텍스트 가져오기
             link = cartoon_title.find("a").get("href")  # <a href = ""> text </a> - 테그의 속성값 가져오기
             link = "https://comic.naver.com"+link
-            print(title)
-            print(link)
+            # print(title)
+            # print(link)
+            html += "<a href = '{}'>{}</a><br/>".format(link,title) # <a href = 'link'>title</a>
+        html += "</body></html>"
+        # print(html)
+
+        with open("간떨어지는동거.html","w",encoding="utf-8") as f :   # html파일 만들기
+            f.write(html)
