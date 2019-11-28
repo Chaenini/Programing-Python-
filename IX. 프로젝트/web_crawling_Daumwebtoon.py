4a7037063955ad01240eb4e2f36b88e803e6154c
Chaenini <s2018w16@e-mirim.hs.kr>
2019-11-28 09:55:25 +0900

    Web Crawling - 다음웹툰 취향저격그녀 json, html

--- IX. 프로젝트/web_crawling_Daumwebtoon.py
+++ IX. 프로젝트/web_crawling_Daumwebtoon.py
@@ -12,15 +12,25 @@ if __name__ == '__main__' :
         with urlopen("http://webtoon.daum.net/data/pc/webtoon/view/favorite") as data:
             j = json.loads(data.read())    #httpResponse -> json
 
+
+        html = "<html><head><meta charset = 'utf-8'></head><body>"
+
         cartoon_titles = j["data"]["webtoon"]["webtoonEpisodes"]    # data > webtoon > webtoonEpisodees
         for cartoon_title in cartoon_titles :   #리스트
             title = cartoon_title["title"]  #타이틀 가져오기
-            print(title)
+
             thumbnail = cartoon_title["thumbnailImage"]["url"]
-            print(thumbnail)
+
             url = cartoon_title["id"]
             url = "http://webtoon.daum.net/webtoon/viewer/"+str(url)
-            print(url)
+            html += "<a href='{}'><img src = '{}' />{}</a>".format(url,thumbnail,title)
+
+        html += "</body></html>"
+
+        outputsoup = BeautifulSoup(html, "lxml")    # 내가 생성한 html 문자열을 soup 객체로 만들기
+        prettyHtml = str(outputsoup.prettify())     # 예쁘게 html 코드 만들기
+        with open("취향저격그녀.html","w",encoding="utf-8") as f :   # html파일 만들기
+            f.write(prettyHtml)
 
 
 
