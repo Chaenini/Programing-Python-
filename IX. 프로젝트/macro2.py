e3e1b606ae644782107fa7e86ee0cf530e502f02
Chaenini <s2018w16@e-mirim.hs.kr>
2019-12-09 08:42:18 +0900

    pyautoGUI - keyboard

---
+++ IX. 프로젝트/macro2.py
@@ -0,0 +1,14 @@
+import pyautogui as pag
+import time
+
+if __name__ == '__main__' :
+    pag.moveTo(200,200,2)
+    pag.click()
+    # pag.typewrite("Happy BirthdayDay JUDY",interval=0.5)
+    pag.typewrite("Happy BirthdayDay JUDY")
+    pag.press("enter")
+    pag.typewrite("Happy BirthdayDay NICK")
+    pag.press("enter")
+    pag.press("hangul")
+    pag.typewrite("ekdtlsdml toddlfdmf cnrgkgkqslek !!")
+    pag.press("hangul")
